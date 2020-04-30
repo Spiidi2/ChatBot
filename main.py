07150878332a4c75baeaf6abb4a438d63c4fe808
Spiidi2 <53562971+Spiidi2@users.noreply.github.com>
2020-04-30 13:50:53 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+#Importing the libraries
 import nltk
 import numpy
 import tflearn
@@ -8,11 +9,13 @@ import pickle
 from nltk.stem.lancaster import LancasterStemmer
 stemmer = LancasterStemmer()
 
+# Opening the json file, which we use
 with open("intents.json") as file:
     data = json.load(file)
 
+# Looping trough the data
 try:
-
+    # Loading existing model/file if it exist
     with open("data.pickle", "rb") as f:
         words, labels, training, output = pickle.load(f)
 except:
@@ -31,16 +34,20 @@ except:
         if intent["tag"] not in labels:
             labels.append(intent["tag"])
 
+    # Ignoring "?" in patterns.
     words = [stemmer.stem(w.lower()) for w in words if w != "?"]
-    words = sorted(list(set(words)))
 
+    # Sort and remove duplicates
+    words = sorted(list(set(words)))
     labels = sorted(labels)
 
+# Checking the labels
     training = []
     output = []
 
     out_empty = [0 for _ in range(len(labels))]
 
+# Checking the patterns if there is the word or there is none
     for x, doc in enumerate(docs_x):
         bag = []
 
@@ -52,16 +59,18 @@ except:
             else:
                 bag.append(0)
 
+        # Checking the tag row
         output_row = out_empty[:]
         output_row[labels.index(docs_y[x])] = 1
 
         training.append(bag)
         output.append(output_row)
 
-
+    # Taking the lists and changing them into arrays
     training = numpy.array(training)
     output = numpy.array(output)
 
+    # Opening the data file
     with open("data.pickle", "wb") as f:
         pickle.dump((words, labels, training, output), f)
 
@@ -70,14 +79,17 @@ try:
 except:
     tensorflow.reset_default_graph()
 
+    # Getting the probability of the word with the use of neurons in neural network
     net = tflearn.input_data(shape=[None, len(training[0])])
     net = tflearn.fully_connected(net, 8)
     net = tflearn.fully_connected(net, 8)
     net = tflearn.fully_connected(net, len(output[0]), activation='softmax')
     net = tflearn.regression(net)
 
+    # Training the model
     model = tflearn.DNN(net)
 
+    # Passing the training data and saving it
     model.fit(training, output, n_epoch=1000, batch_size=8, show_metric=True)
     model.save("model.tflearn")
 
@@ -85,6 +97,7 @@ except:
 def bag_of_words(s, words):
     bag = [0 for _ in range(len(words))]
 
+    # Getting the list of tokenized words
     s_words = nltk.word_tokenize(s)
     s_words = [stemmer.stem(word.lower()) for word in s_words]
 
@@ -103,10 +116,13 @@ def chat():
         if inp.lower() == "quit":
             break
 
+        # Getting the probability of input. And outputting the highest probability output.
         results = model.predict([bag_of_words(inp, words)])
         results_index = numpy.argmax(results)
+        # Getting the probability of the tag from intent
         tag = labels[results_index]
 
+        # Randomly picks response of the highest probability tag
         for tg in data["intents"]:
             if tg['tag'] == tag:
                 responses = tg['responses']
